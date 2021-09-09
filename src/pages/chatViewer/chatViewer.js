9c047f0c0091c90499d828c083890a40d9ef4d4b
xwvike <xwvike@gmail.com>
2021-09-09 17:35:59 +0800

    新增功能

--- src/pages/chatViewer/chatViewer.js
+++ src/pages/chatViewer/chatViewer.js
@@ -3,53 +3,87 @@ import './chatViewer.scss'
 import io from 'socket.io-client'
 import config from '../../config'
 import SingleMsg from './singleMsg/singleMsg'
-import LivePlay from './livePlay/livePlay';
+import LivePlay from './livePlay/livePlay'
 import PlayControl from './playControl/playControl'
 import Axios from 'axios'
-import { Row, Col, Select, message, Modal, Button, Input } from 'antd'
-let socket = null,socketTwo = null;
+import { Row, Col, Select, message, Modal, Button, Input,Tabs,Radio,Space } from 'antd'
+import axios from 'axios'
+const { TabPane } = Tabs;
+let num = 0;
 
+function debounce(fn, delay = 500) {
+  // timer 是在闭包中的
+  let timer = null;
+
+  return function() {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+      timer = null
+    }, delay)
+  }
+}
+
+let socket = null, socketTwo = null, move = false,start = 0,offSetX = 0;
 
 function gql (query) {
   return query[0]
 }
 
-class ChatViewer extends React.Component {
+class ChatViewer extends React.PureComponent {
   constructor () {
     super()
     this.state = {
       chatList: [],
       count: 0,
       hot: 0,
-      uid:'',
-      oldCount:0,
-      oldHot:0,
+      uid: '',
+      oldCount: 0,
+      oldHot: 0,
       mobile: '',
       pwd: '',
       courseList: [],
       username: '',
       loginVisible: false,
       avatar: '',
-      width:'',
-      add:false,
+      width: '',
+      playWidth: 500,
+      add: false,
+      notice:'',
+      oldNotice:'',
+      selectNotice:'1'
     }
     this.changeCourse = this.changeCourse.bind(this)
     this.init = this.init.bind(this)
     this.login = this.login.bind(this)
     this.logout = this.logout.bind(this)
     this.showLogin = this.showLogin.bind(this)
+    this.drag = this.drag.bind(this)
+    this.getNotice = this.getNotice.bind(this)
   }
 
   componentDidMount () {
-    this.setState({width:document.body.offsetWidth})
-    window.addEventListener('resize',_=>{
-      this.setState({width:document.body.offsetWidth})
+    this.setState({
+      width: document.body.offsetWidth,
+      avatar: window.sessionStorage.getItem('avatar'),
+      username: window.sessionStorage.getItem('nickName'),
+      uid: window.sessionStorage.getItem('token') || ''
+    })
+    window.addEventListener('resize', _ => {
+      this.setState({ width: document.body.offsetWidth })
+    })
+    document.addEventListener('mouseup',_=>{
+      this.endDrag(_)
+    })
+    document.addEventListener('mousemove',_=>{
+      this.drag(_)
     })
     this.getCourseList()
   }
 
-
-  init (courseId, lessonId, channel, uid, username,channelId) {
+  init (courseId, lessonId, channel, uid, username, channelId) {
     const data = {
       forceNew: true,
       reconnectionDelay: 2000,
@@ -61,23 +95,35 @@ class ChatViewer extends React.Component {
         lessonId,
         uid,
         channel,
-        username:''
+        username: ''
       }
     }
     if (socket !== null) {socket.close()}
     if (socketTwo !== null) {socketTwo.close()}
     socket = io(config.ioUrl, data)
-    socketTwo = io('https://wss.xintujing.cn/chat',{
+    socketTwo = io('https://wss.xintujing.cn/chat', {
       transports: ['websocket']
     })
-    socketTwo.on('connect',_=>{
+    socketTwo.on('connect', _ => {
       let charRoomLoginInfo = {
         userId: ``,
         room: channelId,
         username: ``,
         level: '4'
-      };
+      }
       socketTwo.emit('login', charRoomLoginInfo)
+      socketTwo.on('login',_=>{
+        if(num<=0){
+          socketTwo.emit('heat')
+          socketTwo.emit('received message')
+          socketTwo.emit('receive flower')
+          socketTwo.emit('get all main point')
+          socketTwo.emit('get announcement')
+          socketTwo.emit('get banned time')
+          socketTwo.emit('get question')
+          num+=1
+        }
+      })
     })
     socket.on('connect', _ => {
       message.success('连接成功')
@@ -86,7 +132,7 @@ class ChatViewer extends React.Component {
       console.log(_)
     })
     socket.on('notify', _ => {
-      console.log(_)
+      this.setState({notice:_})
     })
     socket.on('connect_error', _ => {
       console.log(_)
@@ -98,7 +144,7 @@ class ChatViewer extends React.Component {
       console.log(_)
     })
     socket.on('con_error', _ => {
-      console.log(_)
+      message.error(_ + '请退出重新登录！')
     })
     socket.on('user join', _ => {
       console.log(_)
@@ -109,8 +155,8 @@ class ChatViewer extends React.Component {
     socket.on('heat', _ => {
       this.setState({ hot: _ })
     })
-    socketTwo.on('heat',_=>{
-      this.setState({oldHot:_})
+    socketTwo.on('heat', _ => {
+      this.setState({ oldHot: _ })
     })
     socket.on('msg', e => {
       let obj = {}
@@ -129,13 +175,13 @@ class ChatViewer extends React.Component {
         ele.scrollTop = ele.scrollHeight
       }, 0)
     })
-    socketTwo.on('received message',_=>{
-      let obj = {};
-      obj.id=Math.random().toString().substr(2) + new Date().getTime();
+    socketTwo.on('received message', _ => {
+      let obj = {}
+      obj.id = Math.random().toString().substr(2) + new Date().getTime()
       obj.g = _.msg
       obj.p = '0'
       obj.u = _.user.username
-      obj.c = _.user.level==='2'?2:_.user.level==='3'?3:0
+      obj.c = _.user.level === '2' ? 2 : _.user.level === '3' ? 3 : 0
       obj.s = '1'
       this.setState({ chatList: [...this.state.chatList, obj] })
       setTimeout(() => {
@@ -143,11 +189,18 @@ class ChatViewer extends React.Component {
         ele.scrollTop = ele.scrollHeight
       }, 0)
     })
-    socketTwo.on('receive flower',_=>{
-      let obj = {};
-      obj.id = Math.random().toString().substr(2) + new Date().getTime();
-      obj.g='一朵'
-      obj.p='1'
+    socketTwo.on('get announcement',_=>{
+      this.setState({oldNotice:_.msg})
+    })
+    socketTwo.on('receive announcement',_=>{
+      this.setState({oldNotice:_.msg})
+    })
+
+    socketTwo.on('receive flower', _ => {
+      let obj = {}
+      obj.id = Math.random().toString().substr(2) + new Date().getTime()
+      obj.g = '一朵'
+      obj.p = '1'
       obj.giftMsg = '送给老师'
       obj.u = _.user.username
       obj.c = 0
@@ -159,14 +212,14 @@ class ChatViewer extends React.Component {
         ele.scrollTop = ele.scrollHeight
       }, 0)
     })
-    socketTwo.on('interaction number',_=>{
-      let obj = {};
-      obj.id=Math.random().toString().substr(2) + new Date().getTime();
-      obj.g = _.type===0?'老师好':_.type===1?'明白了':'还不懂'
+    socketTwo.on('interaction number', _ => {
+      let obj = {}
+      obj.id = Math.random().toString().substr(2) + new Date().getTime()
+      obj.g = _.type === 0 ? '老师好' : _.type === 1 ? '明白了' : '还不懂'
       obj.p = '1'
       obj.giftMsg = ''
       obj.u = _.user.username
-      obj.f = _.type===0?'lsh':_.type===1?'mbl':'hbd'
+      obj.f = _.type === 0 ? 'lsh' : _.type === 1 ? 'mbl' : 'hbd'
       obj.c = 0
       obj.s = '1'
       this.setState({ chatList: [...this.state.chatList, obj] })
@@ -178,8 +231,8 @@ class ChatViewer extends React.Component {
     socket.on('count', _ => {
       this.setState({ count: _ })
     })
-    socketTwo.on('online count',_=>{
-      this.setState({oldCount:_})
+    socketTwo.on('online count', _ => {
+      this.setState({ oldCount: _ })
     })
   }
 
@@ -192,6 +245,23 @@ class ChatViewer extends React.Component {
     })
     this.setState({ courseList: list })
     return ''
+  }
+
+  async getNotice(id=''){
+    let result = await axios.post('https://api.xtjzx.cn/index',{
+      operationName:'CourseNoticeInfo',
+      query: gql`
+        query CourseNoticeInfo($id:Long){
+            CourseNoticeInfo(input: {id: $id}){
+                noticeInfo{
+                    content
+                }
+            }
+        }
+      `,
+      variables: { id }
+    })
+    this.setState({notice:result.data.data.data.CourseNoticeInfo.noticeInfo.content})
   }
 
   async login (mobile = this.state.mobile, pwd = this.state.pwd) {
@@ -209,14 +279,16 @@ class ChatViewer extends React.Component {
       `,
       variables: { appCode: 104, mobile, pwd }
     }).catch((err) => {return Promise.reject(err)})
-    if(result.data.code==='400'){
+    if (result.data.code === '400') {
       message.error(result.data.msg)
       return
     }
     message.success('登录成功')
 
     const { nickName, token, avatar, userType } = result.data.data.data.LoginByAccount
-
+    window.sessionStorage.setItem('token', token)
+    window.sessionStorage.setItem('avatar', avatar)
+    window.sessionStorage.setItem('nickName', nickName)
     this.setState({
       uid: token,
       username: nickName,
@@ -229,14 +301,17 @@ class ChatViewer extends React.Component {
 
   async logout () {
     this.setState({ uid: '', mobile: '', pwd: '', liveUrl: '', chatList: [], hot: 0, count: 0 })
-    try{
+    try {
       socket.close()
-    }catch (e) {}
+    } catch (e) {}
+    sessionStorage.clear()
     message.success('登出成功')
   }
-  showLogin(){
-    this.setState({loginVisible:true})
+
+  showLogin () {
+    this.setState({ loginVisible: true })
   }
+
   async changeCourse (e) {
     let result = await Axios.post('https://api.xtjzx.cn/index', {
       operationName: 'chapterListCached',
@@ -267,23 +342,51 @@ class ChatViewer extends React.Component {
               ' {courseId:$courseId,lessonId: $lessonId}){↵            courseLive{↵                avChatRoomId↵                channelId↵                name↵                playUrl↵            }↵            courseVideo{↵                chatHistory↵                lowQualityUrl↵                normalQualityUrl↵                highQualityUrl↵            }↵        }↵    }',
       variables: { lessonId, courseId: e.toString() },
     })
-    this.setState({ liveUrl: url.data.data.data.getCourseVideo.courseLive.playUrl })
-    let data =  url.data.data.data.getCourseVideo.courseLive
-    this.init(e, lessonId,data.avChatRoomId, this.state.uid, this.state.username,data.channelId)
+    await this.getNotice(e.toString())
+    this.setState({ liveUrl: url.data.data.data.getCourseVideo.courseLive.playUrl,courseId:e.toString() })
+    let data = url.data.data.data.getCourseVideo.courseLive
+    this.init(e, lessonId, data.avChatRoomId, this.state.uid, this.state.username, data.channelId)
+  }
+
+  startDrag (e) {
+    move = true
+    offSetX = e.nativeEvent.offsetX
+  }
+
+  endDrag () {
+    move = false
+  }
+
+  drag (e) {
+    if (move) {
+      let pageX = e.pageX;
+      this.setState({playWidth:pageX - offSetX})
+    }
   }
 
   render () {
     return (
             <div className={'all'}>
+              <div className={'drag'} onMouseDown={this.startDrag.bind(this)}  style={{ left: this.state.playWidth + 'px' }}/>
               <div className={'chatBox'}>
-                <div className={'livePlay'}>
+                <div className={'livePlay'} style={{ width: this.state.playWidth + 'px' }}>
                   <div className={'live'}>
                     <LivePlay id={'courseOne'} liveUrl={this.state.liveUrl}/>
                   </div>
-                  <PlayControl oldHot={this.state.oldHot} oldCount={this.state.oldCount} showLogin={this.showLogin}  uid={this.state.uid} selectLive={this.changeCourse}
+                  <PlayControl id={this.state.courseId} oldHot={this.state.oldHot} oldCount={this.state.oldCount} showLogin={this.showLogin}
+                               uid={this.state.uid} selectLive={this.changeCourse}
                                hot={this.state.hot} num={this.state.count} courseList={this.state.courseList}/>
                 </div>
-                <div className={'chat'}>
+                <div className={'chat'} style={{width:`calc(100% - ${this.state.playWidth+10}px)`}}>
+                  <div className={'gg'}>
+                    <Radio.Group  defaultValue='1' size={'small'} onChange={_=>this.setState({selectNotice:_.target.value})}>
+                      <Radio.Button value='1'>新途径在线</Radio.Button>
+                      <Radio.Button value='2'>新途径教育</Radio.Button>
+                    </Radio.Group>
+                    <div className={'notice'}>
+                      {this.state.selectNotice==='1'?this.state.notice:this.state.oldNotice}
+                    </div>
+                  </div>
                   <div onClick={() => this.setState({ loginVisible: !this.state.loginVisible })} className={'other'}/>
                   <SingleMsg chatList={this.state.chatList}/>
                 </div>
--- src/pages/chatViewer/playControl/playControl.js
+++ src/pages/chatViewer/playControl/playControl.js
@@ -1,54 +1,85 @@
-import React from 'react';
+import React from 'react'
 import './playControl.scss'
-import { Button, Col, Row, Select } from 'antd'
+import { Button, Col, Row, Select, Input,message } from 'antd'
 import { FireTwoTone, SmileTwoTone } from '@ant-design/icons'
 import logo from '../../../image/18.png'
 import logoT from '../../../image/9.png'
+import axios from 'axios'
 
-const {Option} = Select;
+const { Option } = Select
+const {TextArea} = Input;
 
-class PlayControl extends React.Component{
-  constructor () {super()}
+class PlayControl extends React.Component {
+  constructor (props) {
+    super()
+    this.state = {
+      inputNotice:''
+    }
+  }
+
+  setNotice(){
+    if(this.state.inputNotice===''){
+      message.info('请输入内容后更新公告')
+      return
+    }
+    axios.get(`https://api.xtjzx.cn/course_manager/api/course/notice?course_id=${this.props.id}&content=${this.state.inputNotice}`).then(_=>{
+      message.success('更新成功,稍后将展示')
+      this.setState({inputNotice:''})
+    })
+  }
+
   render () {
-    const { hot, num, courseList, selectLive, uid,showLogin,oldHot,oldCount } = this.props
-    return(
+    const { hot, num, courseList, selectLive, uid, showLogin, oldHot, oldCount } = this.props
+    return (
             <div className={'liveInfo'}>
-              {uid===''?<Button onClick={showLogin} type={'primary'}>登录</Button>:
-              <>
-                <Row style={{marginBottom:'10px'}} align={'middle'} gutter={20}>
-                  <Col><img className={'logo'} src={logo}/> 新途径在线</Col>
-                  <Col>
-                    <div className={'hot'}><FireTwoTone/>热度：{hot}</div>
-                  </Col><Col>
-                  <div className={'hot'}><SmileTwoTone/>人数：{num}</div>
-                </Col>
-                </Row>
-                <Row align={'middle'} gutter={20}>
-                  <Col><img className={'logo'} src={logoT}/> 新途径教育</Col>
-                  <Col>
-                    <div className={'hot'}><FireTwoTone/>热度：{oldHot}</div>
-                  </Col><Col>
-                  <div className={'hot'}><SmileTwoTone/>人数：{oldCount}</div>
-                </Col>
-                </Row>
-                <div className={'liveSelect'}>
-                  <Row gutter={20} align={'middle'}>
-                    <Col><Select
-                            showSearch
-                            style={{ width: 300 }}
-                            placeholder="选择一个课程"
-                            optionFilterProp="children"
-                            onChange={selectLive}
-                            filterOption={(input, option) =>
-                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                            }
-                    >
-                      {courseList.map(item => {
-                        return <Option value={item.Id} key={item.Id}>{item.name}</Option>
-                      })}
-                    </Select></Col>
-                  </Row>
-                </div></>
+              {uid === '' ? <Button onClick={showLogin} type={'primary'}>登录</Button> :
+                      <>
+                        <div className={'liveSelect'}>
+                          <h3>课程选择</h3>
+                          <Row gutter={20} align={'middle'}>
+                            <Col><Select
+                                    showSearch
+                                    style={{ width: 300 }}
+                                    placeholder="选择一个课程"
+                                    optionFilterProp="children"
+                                    onChange={selectLive}
+                                    filterOption={(input, option) =>
+                                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                    }
+                            >
+                              {courseList.map(item => {
+                                return <Option value={item.Id} key={item.Id}>{item.name}</Option>
+                              })}
+                            </Select></Col>
+                          </Row>
+                        </div>
+                        <Row>
+                          <h3>热度信息</h3>
+                        </Row>
+                        <Row style={{ marginBottom: '10px' }} align={'middle'} gutter={20}>
+                          <Col><img className={'logo'} src={logo}/> 新途径在线</Col>
+                          <Col>
+                            <div className={'hot'}><FireTwoTone/>热度：{hot}</div>
+                          </Col>
+                          <Col>
+                            <div className={'hot'}><SmileTwoTone/>人数：{num}</div>
+                          </Col>
+                        </Row>
+                        <Row align={'middle'} gutter={20}>
+                          <Col><img className={'logo'} src={logoT}/> 新途径教育</Col>
+                          <Col>
+                            <div className={'hot'}><FireTwoTone/>热度：{oldHot}</div>
+                          </Col><Col>
+                          <div className={'hot'}><SmileTwoTone/>人数：{oldCount}</div>
+                        </Col>
+                        </Row>
+
+                        <div className={'noticeSet'}>
+                          <h3>公告设置</h3>
+                          <TextArea placeholder={'输入内容以更新新途径在在线公告'} value={this.state.inputNotice} onChange={_=>this.setState({inputNotice:_.target.value})} showCount maxLength={200}/>
+                          <Button onClick={this.setNotice.bind(this)} style={{marginTop:'5px'}} type="primary">更新公告</Button>
+                        </div>
+                      </>
               }
             </div>
     )
